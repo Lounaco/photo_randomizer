e652883040b308236850063aafc13d8e09b99a0e
Lounaco <squirrelonair@gmail.com>
2023-02-22 15:37:49 +0300

    "22_02"

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//start
+
 const section = document.querySelector("section");
 const playerLivesCount = document.querySelector ("span");
 const playerLives = 6;
+
+//link text
+playerLivesCount.textContent = playerLives;
+
+//generate the data
+
+[{imgSrc: './images/'}]
