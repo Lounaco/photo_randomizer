b6a4c77c781837fe204932b45d31733695dddd2e
Kristina <squirrelonair@gmail.com>
2024-06-11 15:46:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-//start
+// start
 const section = document.querySelector("section");
-const playerLivesCount = document.querySelector ("span");
+const playerLivesCount = document.querySelector("span");
 const playerLives = 6;
 
-//link text
-playerLivesCount.textContent = playerLives;
+// link text
+if (playerLivesCount) {
+    playerLivesCount.textContent = playerLives;
+}
 
-//generate the data
-const getData = () =>  [
+// generate the data
+const getData = () => [
     { imgSrc: "./images/7C2A0947.jpg", name: "dandelion" },
     { imgSrc: "./images/95E36905.jpg", name: "maple leaf" },
     { imgSrc: "./images/99A0E241.jpg", name: "figs tree" },
@@ -26,37 +28,36 @@ const getData = () =>  [
     { imgSrc: "./images/IMG_6763.jpg", name: "trees" },
 ];
 
-//randomize
+// randomize
 const randomize = () => {
     const cardData = getData();
-    cardData.sort( () => Math.random() -0.5);
+    cardData.sort(() => Math.random() - 0.5);
     return cardData;
-
 };
 
-//cards
+// cards
 const cardGenerator = () => {
     const cardData = randomize();
 
-//html generate
+    // html generate
     cardData.forEach((item) => {
-    const card = document.createElement("div");
-    const face = document.createElement("img");
-    const back = document.createElement("div");
-    card.classlist ="card";
-    face.classlist = "face";
-    back.classlist = "back";
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList.add("card");
+        face.classList.add("face");
+        back.classList.add("back");
 
-//info for cards
-    face.src = item.imgSrc;    
+        // info for cards
+        face.src = item.imgSrc;
 
-//section for cards
-    section.appendChild(card);
-    card.appendChild(face);
-    card.appendChild(back);
-
+        // section for cards
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
     });
-
 };
 
-cardGenerator();
+if (section) {
+    cardGenerator();
+}
